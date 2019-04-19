baa5b516c231307f9ebd0f2cfdb96ce220be9fdb
StevanCakic <stevancakic93@gmail.com>
2019-04-19 21:47:35 +0200

    update content for the first coding class

--- Week 1/code-1.py
+++ Week 1/code-1.py
@@ -1,6 +1,7 @@
 # Prvi dio
 # Varijable
 
+# Zadatak 1
 # Kreirati sledeće varijable:
     # ime koja sadrži vaše ime
     # prezime koja sadrži vaše prezime
@@ -20,7 +21,7 @@
         # Kako da nađete korijen broja koristeći ** operator?
     # Isprobati operator eksponent (e)
         # Šta je rezultat 4e2
-
+# Zadatak 2
 # Ako je cijena računara 400e, štampati koliko treba odvojite za plaćanje PDVa
 # Štampati rezultat u sledećem formatu:
     # Cijena računara je 400e. Iznos PDVa je {pdv}, a cijena računara bez PDVa je {cijena bez PDVa}
@@ -37,7 +38,38 @@
 # Četvri dio
 # Stringovi
 
+# a)
 # Testirati len(str), str[index]
+# upper(), split(), capitalize()
+
+# Zadatak 3
+# Iz unijetog stringa izvuci svaki treci karakter za prvih 10 karaktera stringa
+
+# b)
+# Jos primjera
+# a = "fafafafafafaf"
+# print(a.istitle())
+# print(a.count("fa"))
+
+
+
+# Peti dio
+# Print formatting
+
+# print("korisnik ima %d godina" %12)
+# print("student ima prosjek %1.5f godina na %d. je godini." %(7.121216, 1))
+# (7.121216, 1) - tuple (brzo cemo govoriti o tome)
+# Sta ako stavimo da je broj na koji zaokruzujemo veci od broja cifara nakon decimalne tacke?
+# Sta ako povecamo prvi broj pri formatiranju?
+
+# Da li je moguce: print("abc" + 1)
+# Testirati: print("String %s" %(123))
+
+# print ("First: %s, Second: %s" %(2, 2))
+# print ("First: {x}, Second: {x}".format(x=2))
+
+# print ("First: {x}, Second: {y}, Third: {x}".format(x=2, y=3))
+
 
 
 
