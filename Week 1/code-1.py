ebf5199ef3570815839cf23a22482200e3563b33
stevan.cakic <stevan.cakic@itsystems.me>
2019-04-19 15:33:58 +0200

    commit for the first python class

---
+++ Week 1/code-1.py
@@ -0,0 +1,43 @@
+# Prvi dio
+# Varijable
+
+# Kreirati sledeće varijable:
+    # ime koja sadrži vaše ime
+    # prezime koja sadrži vaše prezime
+    # godine koja sadrži vaše godine
+    # email koja sadrži vašu email adresu
+# Štampati rezultat u sledećem formatu:
+    # Email adresa korisnika {ime} {prezime} ({godine}) je {email}.
+
+
+
+# Drugi dio
+# Brojevi
+
+# Isprobajte aritmetičke operacije sa integer i floating brojevima. 
+    # Sta je rezultat operacije 0.1 + 0.2?
+    # Isporbati operator **
+        # Kako da nađete korijen broja koristeći ** operator?
+    # Isprobati operator eksponent (e)
+        # Šta je rezultat 4e2
+
+# Ako je cijena računara 400e, štampati koliko treba odvojite za plaćanje PDVa
+# Štampati rezultat u sledećem formatu:
+    # Cijena računara je 400e. Iznos PDVa je {pdv}, a cijena računara bez PDVa je {cijena bez PDVa}
+
+
+
+# Treci dio
+# Casting
+
+# Isprobati par konverzija iz jednog tipa podatka u drugi
+
+
+
+# Četvri dio
+# Stringovi
+
+# Testirati len(str), str[index]
+
+
+
