ce0fa1b5967d11a0593b6b675749201d50537650
StevanCakic <stevancakic93@gmail.com>
2019-04-24 13:43:20 +0200

    functions, loops, exercises

--- Week 2/exercises_1.py
+++ Week 2/exercises_1.py
@@ -2,7 +2,14 @@
 # Korisnik unosi tri broja. 
 # Naći minimum i maksimum među unijetim brojevima i rezultat prikazati korisniku
 
-# Zadatak 2
+# Zadatak 2 
+# Napisati program koji racuna zbir parnih i proizvod neparnih brojeva od 1 do 15
+# Takodje, prikazati koliko ima parnih, a koliko neparnih brojeva iz tog segmenta
+
+# Zadatak 3
+# Kreirati program koji stampa elemente matrice po vrstama. Matricu prethodno kreirati
+
+# Zadatak 4
 # Napisati program koja za unijeti URL (string), izvlači (parsira) samo domain name i
 # vraće ga kao string. Pretpostaviti da korisnik unosi ispravan URL.
 
---
+++ Week 2/functions.py
@@ -0,0 +1,23 @@
+# Funkcije
+
+def add_number(param1, param2):
+    return param1 + param2
+
+def print_country(country = "Crna Gora"):
+  print("Ja sam iz " + country)
+
+def print_food(food):
+  for x in food:
+    print(x)
+
+# print(add_number())
+# print(add_number(1))
+# print(add_number(1,2))
+
+# print_country()
+
+print_country("Italija")
+
+fruits = ["apple", "banana", "cherry"]
+
+print_food(fruits)
