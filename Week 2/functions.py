cc155ca6e4b5f65f40320b1f2a0d814e45d61774
StevanCakic <stevancakic93@gmail.com>
2019-04-24 13:56:51 +0200

    extend examples for functions

--- Week 2/functions.py
+++ Week 2/functions.py
@@ -3,21 +3,24 @@
 def add_number(param1, param2):
     return param1 + param2
 
+# print(add_number())
+# print(add_number(1))
+# print(add_number(1,2))
+
 def print_country(country = "Crna Gora"):
   print("Ja sam iz " + country)
+
+# print_country()
 
 def print_food(food):
   for x in food:
     print(x)
 
-# print(add_number())
-# print(add_number(1))
-# print(add_number(1,2))
+# print_country("Italija")
+# fruits = ["apple", "banana", "cherry"]
+# print_food(fruits)
 
-# print_country()
+def ime_prezime(ime="Stevan", prezime="Čakić"):
+    return f'{ime} {prezime}'
 
-print_country("Italija")
-
-fruits = ["apple", "banana", "cherry"]
-
-print_food(fruits)
+print(ime_prezime(prezime="Prezime", ime="Ime"))
