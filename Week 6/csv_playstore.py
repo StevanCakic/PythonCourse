89c78499766983642fb5b35e5352fc0000bd64b5
StevanCakic <stevancakic93@gmail.com>
2019-05-27 17:09:21 +0200

    small bug fix

--- Week 6/csv_playstore.py
+++ Week 6/csv_playstore.py
@@ -2,6 +2,6 @@ import csv
 
 # Prikazati sve vrste cija je katerogija GAME
 
-# Sortirati sve rekorde po datumima od najnovijeg do najstarijeg, a nakon toga
+# Sortirati sve rekorde po datumima od najnovijeg do najstarijeg(last updated), a nakon toga
 # izdvojiti 10 najnovijih igara
 
