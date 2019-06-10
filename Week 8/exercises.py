d83a3d4f696a2511f63e82d77fa98c8e8c43779b
StevanCakic <stevancakic93@gmail.com>
2019-06-10 21:02:13 +0200

    exercise for articles - week 8 completed

--- Week 8/exercises.py
+++ Week 8/exercises.py
@@ -1,36 +1,83 @@
 from urllib import request
 import json
+import csv
+# Otvoriti url https://api.myjson.com/bins/1cqtp5 (ne zaboraviti read pa decode)
+content = json.loads((request.urlopen("https://api.myjson.com/bins/1cqtp5").read()).decode())
 
-# 1. Otvoriti url https://api.myjson.com/bins/1cqtp5 (ne zaboraviti read pa decode)
-# TODO
+# Napisati funkciju koja vraća article na osnovu vrijednosti atributa title
+def get_article(title):
+    article = list(filter(lambda elem: elem["title"] == title, content["data"]["articles"]))
+    return article[0]
 
-# 2. Napisati sledece funkcije:
-    # 0) Napisati funkciju koja vraća article na osnovu vrijednosti atributa title
-    # a) insert_new_comment (title, author, description)
-        # Omogućava dodavanje komentara. Prije dodavanja komentara odraditi adekvatnu validaciju (title unique). 
-        # Ako je unos ispravan u listu se dodaje novi dict oblika {title, author, description}.               
-        # Napomena: author nije obavezan, tako da ako ga korisnik ne unese, default vrijednost je “anonim”
-    # b) delete_comment_by_title (title)
-        # Brise comment u odnosu na naslov komentara
-    # c) delete_comments_by_author (author)
-        # Brise komentare u odnosu na autora komentara
-    # d) get_comment_by_title (title)
-        # Vraća komentar koji ima vrijednost parametra title
-    # e) get_comments_by_author (author)
-        # Vraća komentare koji imaju vrijednosti za autora vrijednost parametra author
+artikal = get_article("Apple is Listening")
+print(artikal)
 
-# TODO
+'''
+# Napisati funkciju koja: 
+# Omogućava dodavanje komentara. Prije dodavanja komentara odraditi adekvatnu validaciju (title unique). 
+# Ako je unos ispravan u listu se dodaje novi dict oblika {title, author, description}.               
+# Napomena: author nije obavezan, tako da ako ga korisnik ne unese, default vrijednost je “anonim”
+'''
+def insert_new_comment(title, description, author = "anonim"):
+    for comment in artikal["comments"]:
+        if comment["title"] == title:
+            print("Article already exists")
+            return
 
-# Idemo ka prakticnijoj postavci (niko vam u praksi nece reci kako da imenujete funkcije)
-# Takodje, niko vam nece reci sta funkcija treba da radi, sta od podataka da primi, itd
-# 3. Napisati sledece funkcije:
-    # a) Funkcija koja vraća sve articles na osnovu imena autora
-    # b) te article treba upisati u  
-    #   b1) CSV fajl sortirane po broju komentara  
-    #   b2) JSON fajl sortirane po broju pregleda
+    artikal["comments"].append({"title": title, "author": author, "description":description})
 
-# TODO
+# Brise comment u odnosu na naslov komentara
+def delete_comment_by_title(title):
+    for comment in artikal["comments"]:
+        if comment["title"] == title:
+            artikal["comments"].remove(comment)
+            break
 
+# Brise komentare u odnosu na autora komentara
+def delete_comments_by_author(author):
+    for comment in artikal["comments"]:
+        if comment["author"] == author:
+            artikal["comments"].remove(comment)
 
+# Vraća komentar koji ima vrijednost parametra title
+def get_comment_by_title(title):
+    for comment in artikal["comments"]:
+        if comment["title"] == title:
+            return comment
 
+# Vraća komentare koji imaju vrijednosti za autora vrijednost parametra author
+def get_comments_by_author(author):
+    result = []
+    for comment in artikal["comments"]:
+        if comment["author"] == author:
+            result.append(comment)
+    return result
 
+# Funkcija koja vraća sve articles na osnovu imena autora
+def get_articles_by_author(author):
+    articles = list(filter(lambda elem: elem["author"] == author, content["data"]["articles"]))
+    return articles
+
+articles = get_articles_by_author("Marco Arment")
+
+# Odabrane articles (po autoru) treba upisati u CSV fajl sortirane po broju komentara 
+def sort_by_views(article):
+    return article["views"]
+
+sorted_by_views = sorted(articles, key=sort_by_views)
+
+with open('sort_by_views.csv',"w", newline='') as csvfile:
+    fieldnames = ["title", "author", "description", "category", "views", "comments"]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for elem in sorted_by_views:
+        writer.writerow(elem)
+
+def sort_by_comments(article):
+    return len(article["comments"])
+
+# # Odabrane articles (po autoru) treba upisati u JSON fajl sortirane po broju pregleda
+sort_by_comments = sorted(articles, key=sort_by_comments)
+
+with open("sort_by_comments.json", "w", encoding="utf-8") as json_file:
+    json.dump(sort_by_comments, json_file, ensure_ascii=False) 
